e5abe7be13dc7b2d3ef6b0aea68680856aa00bb6
jhowe54 <joseph.howe54@gmail.com>
2021-12-31 12:59:55 -0500

    migrations and seeds working

---
+++ back-end/src/db/migrations/00_20210123122225_createReservationsTable.js
@@ -0,0 +1,18 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("reservations", (table) => {
+    table.increments("reservation_id").primary();
+    table.string("first_name")
+    table.string("last_name")
+    table.string("mobile_number")
+    table.date("reservation_date")
+    table.string("reservation_time")
+    table.integer("people")
+    table.string("status").defaultTo("booked")
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.timestamp("updated_at").defaultTo(knex.fn.now());
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("reservations");
+};
